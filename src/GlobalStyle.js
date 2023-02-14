eeac3bc22a34ce472cac325d4410dd44a038d60d
Iwona Gasidlo <iwona.gasidlo2810@gmail.com>
2023-02-14 12:22:17 +0100

    main css styling moved to GlobalStyle folder

---
+++ src/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        font-family: 'Open Sans', sans-serif;
+        background-color: #eee;
+        word-break: break-word;
+    }
+`;
