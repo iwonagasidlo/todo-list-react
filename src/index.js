ccaec3469173d662b8ccb40d9e5c821f2a1b8861
Iwona Gasidlo <iwona.gasidlo2810@gmail.com>
2023-03-01 15:00:33 +0100

    provider and store file added to index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from "./App";
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
 import { theme } from "./theme";
 import { GlobalStyle } from './GlobalStyle';
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
